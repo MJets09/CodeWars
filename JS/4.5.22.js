05e6f1f9c458ffd43d6215665b4dab3b804defb7
MJets09 <mthaoqk@gmail.com>
2022-04-05 22:11:14 -0500

    Modules

---
+++ JS/4.5.22.js
@@ -0,0 +1,9 @@
+function lovefunc(flower1, flower2) {
+    return flower1 % 2 !== flower2 % 2;
+}
+
+//What was the part that you was stuck on?
+//Figuring out how to properly do module operators, need more practice
+
+//What did you learn?
+//No need for if when you can user ternary operator and not equals since we only have two variables
