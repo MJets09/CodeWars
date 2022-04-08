a0ea345acd7c0861f4ee551e8ba9973e668ddfac
MJets09 <mthaoqk@gmail.com>
2022-04-08 17:12:28 -0500

    array

---
+++ 4.8.22.js
@@ -0,0 +1,15 @@
+function createArray(number) {
+    var newArray = [];
+
+    for (var counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+
+    return newArray;
+}
+
+//What was the part that you was stuck on?
+//No issue
+
+//What did you learn?
+//No issue
