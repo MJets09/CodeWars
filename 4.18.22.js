49d81a36f2828e49139987a1258e120e18bc7b28
MJets09 <mthaoqk@gmail.com>
2022-04-18 22:58:06 -0500

    Array reduce method

---
+++ 4.18.22.js
@@ -0,0 +1,18 @@
+function betterThanAverage(classPoints, yourPoints) {
+    let average = classPoints.reduce((a, b) => a + b) / classPoints.length
+
+    return average < yourPoints ? true : false
+
+}
+
+
+
+
+
+
+//What was the part that you was stuck on?
+//Had to figure out how to obtain the average of the array than compare to my points.
+
+
+//What did you learn?
+//The reduce method, still don't have it completely comprehended. Must work on more, added to Anki
