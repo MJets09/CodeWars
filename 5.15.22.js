18ba5c3d10c36e5476601d061060c35caebffff8
Meng <mthaoqk@gmail.com>
2022-05-15 21:23:04 -0500

    5.15.22

--- 5.15.22.js
+++ 5.15.22.js
@@ -8,6 +8,10 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
+
+
+//Best practice
+
 function countPositivesSumNegatives(input) {
     if (input == null || input.length == 0)
       return [];
@@ -26,4 +30,4 @@ function countPositivesSumNegatives(input) {
     return [positive, negative];
 }
 
-//Couldnt solve byself, had to look at solution. Need to come back and work on more.
+//Couldnt solve byself, had to look at solution. Need to come back and work on more. 
