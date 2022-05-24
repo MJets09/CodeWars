19843d446be92470914c7e44527088888d0629ec
MJets09 <mthaoqk@gmail.com>
2022-05-23 22:27:04 -0500

    5.23.22

---
+++ 5.23.22.js
@@ -0,0 +1,13 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+function noSpace(x) {
+    return x.split(" ").join('')
+}
+
+
+//What was the part that you was stuck on?
+// No trouble, knew of a method that removes characters and join them back together. Had to google,
+// must learn how to remember them.
+
+
+//What did you learn?
+//Nothing to knew, worked on node.js and server side earlier today and took most of my time.
