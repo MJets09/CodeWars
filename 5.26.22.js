4c1a53f7dc891e776d3b71cf728bf37f41a7ecb0
MJets09 <mthaoqk@gmail.com>
2022-05-26 22:09:07 -0500

    5.26.22

---
+++ 5.26.22.js
@@ -0,0 +1,33 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+function maps(x) {
+    return x.map(x => x * 2)
+}
+
+
+//What was the part that you was stuck on?
+//Knew what method to use the map method but couldnt figure out the syntax, sat for awhile
+//had to unlock. Knew map used a function as its argument, 
+
+//What did you learn?
+//You need a lot more practice. Learn to read the syntax online and understand the parameters
+//If you took the time to read the documentation you would of figured it out, you was there.
+//Try one more
+
+console.log(1 * -1)
+
+function hero(bullets, dragons) {
+
+    if (bullets % dragons === 2) {
+        return true
+    } else {
+        return false
+    }
+
+
+}
