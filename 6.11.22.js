662d78f9e6825bfd407a1d76ee042cf41adfab83
Meng <mthaoqk@gmail.com>
2022-06-11 21:26:59 -0500

    6.11.22

---
+++ 6.11.22.js
@@ -0,0 +1,17 @@
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// Expect large Inputs!
+
+function oddCount(n){
+    return Math.floor(n/2);
+  }
+
+  //What was the part that you was stuck on?
+//Didnt know where to start, googled for how to figure out, no results. Had to unlock solutions
+
+
+//What did you learn?
+//Need to understand best solutions, why dividing by 2 works.
